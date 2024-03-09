92ec5a593a20ac1e04bfc1203a58e8cb5c83cc95
Aviv an Ido <ido.ran@gmail.com>
2024-03-09 12:23:40 +0000

    Find car on board
    We advanced in writing the solver algorithm by checking car direction
    and able to scan the board to find all the cells a car has

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ board = \
 board = board.split("\n")
 
 
-cars = ["A","O","Q","P","X"]
+cars = [
+    {"tag": "A", "dir": "hor"},
+    {"tag": "O", "dir": "ver"},
+    {"tag": "Q", "dir": "ver"},
+    {"tag": "P", "dir": "hor"},
+    {"tag": "X", "dir": "hor"},
+]
 
-print(board[6][6])
+for row in range(8):
+    for col in range(8):
+        if board[row][col] == "X":
+            print([row,col])
+
+# for car in cars:
+#     if car["dir"] == "hor":
+#         print("horisantal", car["tag"])
+#     else:
+#         print("vertical", car["tag"])
